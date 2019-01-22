eeef0f3a1963e1a44bfca73815979dcf388c6f8d
James Turello <jturello@yahoo.com>
2019-01-22 17:12:35 -0500

    add morgan middleware for dev env

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 const config = require('config');
 const home = require('./routes/home');
 const courses = require('./routes/courses');
+const morgan = require('morgan');
 const logger = require('./middleware/logger');
+
+console.log(config.get('name'));
 
 app.use(express.json());
 app.use(logger);
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+}
 app.use('/', home);
 app.use('/api/courses', courses);
-
-console.log(config.get('name'));
 
 // PORT
 const port = process.env.PORT || 3000;
