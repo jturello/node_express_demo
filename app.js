773f33dad5dda8cce38d953271d66b867490eade
James Turello <jturello@yahoo.com>
2019-01-22 17:03:43 -0500

    add config files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const app = express();
+const config = require('config');
 const home = require('./routes/home');
 const courses = require('./routes/courses');
 const logger = require('./middleware/logger');
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(logger);
 app.use('/', home);
 app.use('/api/courses', courses);
+
+console.log(config.get('name'));
 
 // PORT
 const port = process.env.PORT || 3000;
